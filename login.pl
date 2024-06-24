48ca91ef4d55ae0aeec1acf373026564bcf4d8fa
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2024-06-23 19:37:36 -0500

    Agregando archivos .pl para inicio y registro de usuario

---
+++ login.pl
@@ -0,0 +1,43 @@
+#!"C:/xampp/perl/bin/perl.exe"
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $cgi = CGI -> new;
+print $cgi->header('text/html; charset=utf-8');
+
+#Parámetros
+my $dsn = 'DBI:mysql:database=gestiondeacreditacion;host=localhost'; #Llenar
+my $username = 'user';
+my $password = '1234';
+
+my $dbh = DBI->connect($dsn, $username, $password) or die "No se pudo conectar a la base de datos";
+
+#Comprobamos si usuario y contraseña están en la base de datos
+my $user = $cgi -> param('usuario');
+my $password = $cgi -> param('contraseña');
+
+my $sth = $dbh->prepare("SELECT * FROM usuario WHERE user = ? AND password = ?");
+$sth->execute($user, $password);
+
+#Si está en la base de datos
+if(my $row = $sth->fetchrow_hashref){
+    print $cgi->redirect('');   #Llenar
+}
+else {
+    print <<HTML;
+        <!DOCTYPE HTML>
+        <html>
+        <head>
+            <title>Error de autenticación</title>
+        </head>
+        <body>
+            <p>Usuario o contraseña incorrectos. Intente de nuevo.</p>
+            <form action="../" method="POST">
+                <input type="submit" value="Regresar">
+            </form>
+        </body>
+        </html>
+HTML
+}
---
+++ register.pl
@@ -0,0 +1,24 @@
+#!"C:/xampp/perl/bin/perl.exe"
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $cgi = CGI -> new;
+print $cgi->header();
+
+#Parámetros
+my $dsn = 'DBI:mysql:database=gestiondeacreditacion;host=localhost';
+my $username = 'user'; 
+my $password = '1234'; 
+
+my $dbh = DBI->connect($dsn, $username, $password) or die "No se pudo conectar a la base de datos";
+
+#Recibimos los datos 
+my $user = $cgi -> param('Usuarios');
+my $name = $cgi -> param('Nombres');
+my $email = $cgi -> param('Correos');
+my $passwd = $cgi -> param('Contraseñas');
+
+my $sth = $dbh->prepare("INSERT INTO usuario (nombre, nombreUsuario, correo, password) VALUES (?, ?, ?, ?)");
+$sth->execute($name, $user, $email, $passwd);
