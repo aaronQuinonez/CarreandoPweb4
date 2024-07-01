d72309a89e102cd285b6fbd0ce710d5077d48668
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2024-06-30 22:59:23 -0500

    Agregando usuario.html admin.html, modificando numero2.html y modificando login.perl para redirigir a admin.html o usuario.html dependiendo de los datos. Se agregó la tabla administradores en la base de datos

--- Proyecto_02/login.pl
+++ Proyecto_02/login.pl
@@ -10,19 +10,22 @@ print $cgi->header('text/html; charset=utf-8');
 #Parámetros
 my $dsn = 'DBI:mysql:database=gestiondeacreditacion;host=localhost';
 my $username = 'user';
-my $password = '1234';
+my $passwd = '1234';
 
-my $dbh = DBI->connect($dsn, $username, $password) or die "No se pudo conectar a la base de datos";
+my $dbh = DBI->connect($dsn, $username, $passwd) or die "No se pudo conectar a la base de datos";
 
 #Comprobamos si usuario y contraseña están en la base de datos
 my $user = $cgi -> param('usuario');
 my $password = $cgi -> param('contraseña');
 
 #Primero comprobamos si los datos pertenecen al administrador
-my $sthadm = $dbh->prepare("SELECT * FROM administradores WHERE nombreUsuario = ? AND password = ?");
-$sthadm->execute($user, $password)
-if(my $rowadm = $sth->fetchrow_hashref){
-    print $cgi->redirect('admin.html');
+my $sthadm = $dbh->prepare("SELECT * FROM administradores WHERE usuario = ? AND password = ?");
+$sthadm->execute($user, $password) or die "Error al ejecutar la consulta SQL para administradores: $DBI::errstr";
+if(my $rowadm = $sthadm->fetchrow_hashref){
+    print "Content-type: text/html\n\n";
+    print "<html><head><title>Redireccionando...</title></head><body>";
+    print "<script>window.location.href = '/PROYECTO%20FINAL%20PWEB/Proyecto_02/admin.html';</script>";
+    print "</body></html>";
 }
 else{
     my $sth = $dbh->prepare("SELECT * FROM usuario WHERE nombreUsuario = ? AND password = ?");
@@ -30,7 +33,10 @@ else{
 
     #Si USUARIO está en la base de datos
     if(my $row = $sth->fetchrow_hashref){
-        print $cgi->redirect('usuario.html');   #Llenar
+        print "Content-type: text/html\n\n";
+        print "<html><head><title>Redireccionando...</title></head><body>";
+        print "<script>window.location.href = '/PROYECTO%20FINAL%20PWEB/Proyecto_02/usuario.html';</script>";
+        print "</body></html>";
     }
     else {
         print <<HTML;
@@ -41,7 +47,7 @@ else{
             </head>
             <body>
                 <p>Usuario o contraseña incorrectos. Intente de nuevo.</p>
-                <form action="index.html" method="POST">
+                <form action="numero2.html" method="POST">
                     <input type="submit" value="Regresar">
                 </form>
             </body>
@@ -49,3 +55,4 @@ else{
 HTML
     }
 }
+$dbh->disconnect;
