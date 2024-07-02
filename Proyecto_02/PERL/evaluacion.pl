fe4725029014c846c793df1817de94415aeb0f9e
Antony <acondoricai@unsa.edu.pe>
2024-07-02 11:56:22 -0500

    Agregando las funciones del admin

---
+++ Proyecto_02/PERL/evaluacion.pl
@@ -0,0 +1,78 @@
+#!"C:/xampp/perl/bin/perl.exe"
+use strict;
+use warnings;
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+use File::Basename;
+use DBI;
+
+# Datos de conexión
+my $username = "user";
+my $password = "1234";
+
+# Conexión a la base de datos
+my $dsn = 'DBI:mysql:database=gestiondeacreditacion;host=localhost';
+my $dbh = DBI->connect($dsn, $username, $password) or die "No se pudo conectar a la base de datos";
+
+# Consulta a la base de datos
+my $sql = "SELECT * FROM solicitudes";
+my $sth = $dbh->prepare($sql);
+$sth->execute();
+
+print "Content-Type: text/html\n\n";
+
+print <<"HTML";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solicitudes</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h1>Lista de Solicitudes</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Institución</th>
+                <th>Dirección</th>
+                <th>Contacto</th>
+                <th>Teléfono</th>
+                <th>Correo</th>
+                <th>Descripción</th>
+                <th>Documento</th>
+                <th>Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+HTML
+
+# Obtención de los datos
+while (my $row = $sth->fetchrow_hashref()) {
+    print <<"HTML";
+            <tr>
+                <td>$row->{id}</td>
+                <td>$row->{institucion}</td>
+                <td>$row->{direccion}</td>
+                <td>$row->{contacto}</td>
+                <td>$row->{telefono}</td>
+                <td>$row->{correo}</td>
+                <td>$row->{descripcion}</td>
+                <td>$row->{documento}</td>
+                <td>$row->{estado}</td>
+            </tr>
+HTML
+}
+
+print <<"HTML";
+        </tbody>
+    </table>
+</body>
+</html>
+HTML
+
+# Cierre de la conexión
+$sth->finish();
+$dbh->disconnect();
