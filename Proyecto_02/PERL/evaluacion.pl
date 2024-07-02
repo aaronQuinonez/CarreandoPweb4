9aaa7075ff178f3defec329cdc7854b5124b9e8c
Antony <acondoricai@unsa.edu.pe>
2024-07-02 12:07:15 -0500

    Agregando la funcion estado

--- Proyecto_02/PERL/evaluacion.pl
+++ Proyecto_02/PERL/evaluacion.pl
@@ -14,6 +14,8 @@ my $password = "1234";
 my $dsn = 'DBI:mysql:database=gestiondeacreditacion;host=localhost';
 my $dbh = DBI->connect($dsn, $username, $password) or die "No se pudo conectar a la base de datos";
 
+my $cgi = CGI->new;
+
 # Consulta a la base de datos
 my $sql = "SELECT * FROM solicitudes";
 my $sth = $dbh->prepare($sql);
@@ -61,11 +63,39 @@ while (my $row = $sth->fetchrow_hashref()) {
                 <td>$row->{correo}</td>
                 <td>$row->{descripcion}</td>
                 <td>$row->{documento}</td>
-                <td>$row->{estado}</td>
+                <td>
+                    <form method="post" action="">
+                        <input type="hidden" name="id" value="$row->{id}">
+                        <select name="nuevo_estado" onchange="this.form.submit()">
+                            <option value="Pendiente" @{[$row->{estado} eq 'Pendiente' ? 'selected' : '']}>Pendiente</option>
+                            <option value="En Proceso" @{[$row->{estado} eq 'En Proceso' ? 'selected' : '']}>En Proceso</option>
+                            <option value="Completado" @{[$row->{estado} eq 'Completado' ? 'selected' : '']}>Completado</option>
+                        </select>
+                    </form>
+                </td>
             </tr>
 HTML
 }
+# Procesa la entrada del formulario
+if ($cgi->param('id') && $cgi->param('nuevo_estado')) {
+    my $id = $cgi->param('id');
+    my $nuevo_estado = $cgi->param('nuevo_estado');
 
+    # Conexión a la base de datos
+    my $dsn = 'DBI:mysql:database=gestiondeacreditacion;host=localhost';
+    my $username = "user";
+    my $password = "1234";
+    my $dbh = DBI->connect($dsn, $username, $password) or die "No se pudo conectar a la base de datos";
+
+    # Actualiza el estado en la base de datos
+    my $update_sql = "UPDATE solicitudes SET estado = ? WHERE id = ?";
+    my $update_sth = $dbh->prepare($update_sql);
+    $update_sth->execute($nuevo_estado, $id);
+
+    # Cierra la conexión
+    $update_sth->finish();
+    $dbh->disconnect();
+}
 print <<"HTML";
         </tbody>
     </table>
