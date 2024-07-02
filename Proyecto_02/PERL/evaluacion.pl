87e21aeb17cfd0ba4b89e906516ee1ecb7c4729f
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2024-07-02 12:43:23 -0500

    Eliminando contenido

--- Proyecto_02/PERL/evaluacion.pl
+++ Proyecto_02/PERL/evaluacion.pl
@@ -28,9 +28,8 @@ print <<"HTML";
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solicitudes</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="../CSS/evaluacion.css">
 </head>
 <body>
     <h1>Lista de Solicitudes</h1>
--- Proyecto_02/PERL/procesar_solicitud.pl
+++ Proyecto_02/PERL/procesar_solicitud.pl
@@ -60,7 +60,6 @@ print <<"HTML";
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estado</title>
     <style>
         body {
